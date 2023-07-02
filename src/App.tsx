5fa5eef14bb5860480d9a43866debd9430adce2b
slice of code <slice_of_code@mail.ru>
2023-07-02 12:07:53 +0400

    второе занятие понедельника с Игорем

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
+
+export type FilterButtonName = "All" | "Active" | "Completed"
+
+function App() {
+
+    const header = "What to learn"
+
+    let [task, setTask] = useState([
+        {id:1, title: "HTML", isDone: true},
+        {id:2, title: "CSS", isDone: true},
+        {id:3, title: "JavaScript", isDone: false},
+        {id:4, title: "React", isDone: false}
+    ])
+
+    const removeTask = (taskID: number) => {
+        task = task.filter(el => el.id !== taskID)
+        setTask(task)
+    }
+
+    return (
+        <div className="App">
+            <Todolist header={header}
+                      tasks={task}
+                      removeTask={removeTask} />
+        </div>
+    );
+}
+
+export default App;
